1b1d5f9ab7a4ea688f7b03524cf6d3d1f5a35c4d
Mercyfulll <mercyful.buthelezi@gmail.com>
2024-02-27 18:32:59 +0200

    third commit

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const choice = document.querySelector(".play");
 const computerChoice = document.querySelectorAll(".icon");
 const elementForPc = document.querySelector(".pc");
 
+var computerScore = 0 
+var playerScore = 0 
+
 pickBtn.forEach(function (buttonPicked) {
     buttonPicked.addEventListener('click', function () {
         var oldElement = choice;
         var newElement = buttonPicked;
+
 
         if (choice) {
             oldElement.parentNode.replaceChild(newElement, oldElement);
@@ -15,8 +19,6 @@ pickBtn.forEach(function (buttonPicked) {
         // Randomly select a computer choice
         var randomIndex = Math.floor(Math.random() * computerChoice.length);
         var randomComputerChoice = computerChoice[randomIndex].cloneNode(true);
-
-        console.log(computerChoice[randomIndex]);
 
         // Create a new button element
         var newButton = document.createElement('button');
@@ -28,13 +30,55 @@ pickBtn.forEach(function (buttonPicked) {
         newImage.classList.add('icon')
         newButton.appendChild(newImage);
 
+        // Extract the value from the imageAttribute source name
+        function extractValueFromSource(source) {
+            const fileName = source.split('/').pop();
+            const value = fileName.split('.')[0].toLowerCase();
+            return value;
+        }
+       
+        const imageValue = extractValueFromSource(newImage.src);
         var pcOldElem = elementForPc;
         var pcNewElem = newButton;
 
+        
+
         if (pcOldElem) {
             var newElem = pcOldElem.parentNode.replaceChild(pcNewElem, pcOldElem)
-            newElem.classList.add('button');
+            newElem.classList.add('btn');
             
         }
-    });
+
+        var imgeAtrribute = newImage.src
+
+        function createSweetAlert(title, icon) {
+            Swal.fire({
+                title: title,
+                icon: icon,
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'btn btn-custom',
+                    title: 'custom-title-class',
+                    popup: 'pop-messages'
+                }
+            });
+        }
+        
+        if (imgeAtrribute.includes(buttonPicked.value)) {
+            createSweetAlert('It\'s a draw', 'info');
+        } else {
+            if ((imgeAtrribute.includes('rock') && buttonPicked.value === 'scissors') ||
+                (imgeAtrribute.includes('scissors') && buttonPicked.value === 'paper') ||
+                (imgeAtrribute.includes('paper') && buttonPicked.value === 'rock')) {
+                    createSweetAlert('Computer wins! ' + capitalizeFirstLetter(imageValue) + ' beats ' + buttonPicked.value, 'warning');
+                } else {
+                    createSweetAlert('You win! ' + capitalizeFirstLetter(buttonPicked.value) + ' beats ' + capitalizeFirstLetter(imageValue), 'success');
+                }
+        }
+        
+        function capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+        
+     });
 });
